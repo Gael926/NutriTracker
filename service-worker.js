8057f80867ca390f7de66a1e5ca5c58fa1573cdb
Gael <glereun.ing2027@esaip.org>
2026-01-30 16:18:17 +0100

    modif eau foncitonnelle + update header

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
    ======================================== */
 
 // Incrémenter à chaque déploiement pour forcer le rafraîchissement du cache
-const CACHE_NAME = 'nutritracker-v21';
+const CACHE_NAME = 'nutritracker-v23';
 
 // Fichiers à mettre en cache
 const urlsToCache = [
